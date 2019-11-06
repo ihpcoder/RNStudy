614e6ad98af00ecfcfd01ddaf58e25a7f2844652
ihpcoder <1175653214@qq.com>
2019-11-06 18:08:12 +0800

    11.6 1

---
+++ Github_RN/ios/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  Github_RN
+//
+//  Created by yuhao yuhao on 2019/11/6.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+
+#endif /* Constants_h */
